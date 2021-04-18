091cd8f4f9dd79b69eae2e3e495a2cc5e6670385
Christopher Girvin <chrisg8691@gmail.com>
2021-04-18 10:28:56 -0400

    basic CPU constructor done; load program functionality completed in ls8.py and cpu.py; simple run function that just reads instruction set from filename argument, loads it into ram and prints the ram out

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -3,9 +3,18 @@
 """Main."""
 
 import sys
-from cpu import *
 
-cpu = CPU()
+from cpu import CPU
 
-cpu.load()
-cpu.run()
+if __name__ == "__main__":
+    # Initialize CPU
+    cpu = CPU()
+
+    # Load instruction set
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 {os.path.basename(__file__)} <path to instruction set>")
+    else:
+        cpu.load(sys.argv[1])
+
+    # Run
+    cpu.run()
